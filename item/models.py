3c87bd4621bdede55bad5abff23c2af0e05fbcc3
calin <sanducalinm@gmail.com>
2023-03-03 11:56:36 +0200

    added_thumbnail fixed: image in frontpage and in dashboard

--- item/models.py
+++ item/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files import File
+from io import BytesIO
+from PIL import Image
 
 # Create your models here.
 
@@ -20,6 +23,7 @@ class Item(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
@@ -27,3 +31,32 @@ class Item(models.Model):
     
     def __str__(self):
         return self.name
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x240.jpg'
+
+    def make_thumbnail(self, image, size=(450, 450)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
+
+    def get_image(self):
+        if self.image:
+            return self.image.url
+        else:
+            return 'https://via.placeholder.com/240x240.jpg'
